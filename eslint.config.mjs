93b3bb7b1a735176d5a93d0922db9357bafc6074
Alvin Hamaïde <alvin98mc@gmail.com>
2025-06-04 01:11:21 +0200

    chore(eslint prettier): updated configs and added plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,55 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { dirname } from "path";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
-  }),
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+	js.configs.recommended,
+	...tseslint.configs.recommended,
+	{
+		files: ["**/*.ts", "**/*.tsx"],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				project: "./tsconfig.json"
+			}
+		}
+	},
+	...compat.config({
+		extends: ["next/core-web-vitals"]
+	}),
+	{
+		plugins: {
+			"simple-import-sort": simpleImportSort
+		},
+		rules: {
+			"simple-import-sort/imports": [
+				"error",
+				{
+					groups: [
+						["^react", "^next", "^@?\\w"],
+						["^@/", "^\\.\\.(?!/?$)", "^\\.\\./?$", "^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+						["^\\u0000"]
+					]
+				}
+			],
+			"simple-import-sort/exports": "error"
+		}
+	},
+	{
+		rules: {
+			...prettier.rules,
+			'import/no-anonymous-default-export': 'off'
+		}
+	},
+	{
+		ignores: ["**/node_modules/**", "**/.next/**", "**/build/**", "**/out/**"]
+	}
 ];
-
-export default eslintConfig;
